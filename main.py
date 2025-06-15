1f0dae2bb47241fd721b33c2672dba91bba37d22
jamesfiddy <j.fiddy89@gmail.com>
2025-06-15 21:04:13 +1000

    Added high score & recording of high score in json file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import json
 from constants import *
 from player import *
 from asteroid import *
@@ -10,8 +11,16 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     score_font = pygame.font.SysFont(None, 40)
+    current_hs_font = pygame.font.SysFont(None, 30, False, True)
     gameover_font = pygame.font.SysFont(None, 100, False, True)
     background_image = pygame.image.load("./assets/img/background.png")
+    high_score = 0
+    new_high_score = False
+
+    # Open the file and load the existing high score - no error handling... yet
+    with open('data.json', 'r') as file:
+        saved_data = json.load(file)
+        high_score = saved_data['high_score']
     
     clock = pygame.time.Clock()
     dt = 0
@@ -34,10 +43,11 @@ def main():
 
     Shot.containers = (updatable, drawable, shots)
 
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                running = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     running = False
@@ -48,6 +58,10 @@ def main():
             for asteroid in asteroids:
                 if asteroid.collision(player):
                     alive = False
+
+                    if score > high_score:
+                        new_high_score = True
+                        high_score = score
             
                 for shot in shots:
                     if asteroid.collision(shot):
@@ -58,14 +72,21 @@ def main():
         screen.fill((0,0,0))
         screen.blit(background_image, (0,0))
 
-        s = score_font.render("Score: " + str(score), True, "White")
-        screen.blit(s, (25,25))
+        score_display = score_font.render("Score: " + str(score), True, "White")
+        screen.blit(score_display, (25,25))
+        highscore_display = current_hs_font.render("High Score: " + str(high_score), True, "White")
+        screen.blit(highscore_display, (SCREEN_WIDTH - 150,25))
 
         if alive:
             for d in drawable:
                 d.draw(screen)
         else: 
-            over = gameover_font.render("Game Over!", True, "White")
+            if new_high_score:
+                highscore_font = pygame.font.SysFont(None, 50, False, True)
+                over = highscore_font.render("Game Over! NEW high score... now saving...", True, "White")
+            else:
+                over = gameover_font.render("Game Over!", True, "White")
+           
             rect = over.get_rect(center=(SCREEN_HEIGHT/2, SCREEN_WIDTH/2))
             screen.blit(over, (rect))
 
@@ -73,6 +94,11 @@ def main():
         
         # Limit frame rate to 180fps
         dt = clock.tick(180) / 1000
+
+    if new_high_score:
+        saved_data = {'high_score': high_score}
+        with open('data.json', 'w') as file:
+            json.dump(saved_data, file, indent=4)
         
 if __name__ == "__main__":
     main()
